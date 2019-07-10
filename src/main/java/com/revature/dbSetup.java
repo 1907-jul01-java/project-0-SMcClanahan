1f4be859391cdef42bd52e62c932c4a30f7d7dab
Sean McClanahan <s.mcclanahan96@gmail.com>
2019-07-09 21:23:49 -0500

    minor changes

--- src/main/java/com/revature/dbSetup.java
+++ src/main/java/com/revature/dbSetup.java
@@ -1,10 +1,36 @@
 package com.revature;
 
-import java.util.Scanner;
+import java.sql.*;
 
-public class dbSetup{
-    private:
-    String url = "";
-    String username = "";
-    String password = "";
+class dbSetup{
+    private String url = "jdbc:postgresql://localhost:5432/Project0db";
+    private String username = "Project0db";
+    private String password = "luft";
+
+    public int connect(String user, String pass){
+        try(Connection connection = DriverManager.getConnection(url, username, password)){
+            Statement statement = connection.createStatement();
+            
+            if(statement.executeQuery("SELECT userName FROM accounts;") != null) {
+                System.out.println("Welcome " + user);
+                return 1;
+            }
+            else if(statement.executeQuery("SELECT userName FROM employees;") != null){
+                return 2;
+            }
+            else if(statement.executeQuery("SELECT userName FROM admin;") != null){
+                System.out.println("Admin status verified on user: "+ user);
+                return 3;
+            }
+            else{
+                System.out.println("User not found");
+                return -1;
+            }
+        } catch(SQLException e){
+
+        }
+        System.out.println("connection failed");
+        return 0;
+    }
+
 }
