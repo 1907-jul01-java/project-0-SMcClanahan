1bce02c2efbac1cf2d4edfccc89a7a85c4bb93a0
Sean McClanahan <s.mcclanahan96@gmail.com>
2019-07-10 19:46:36 -0500

    Added Resources

---
+++ src/main/java/com/revature/Util/ConnectionUtil.java
@@ -0,0 +1,43 @@
+package com.revature.Util;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+class ConnectionUtil {
+    private Connection connection;
+    private String url;
+    private String user;
+    private String password;
+
+    public ConnectionUtil(){
+        try{
+            Properties properties = new Properties();
+            properties.load(new FileReader("resources/connection.properties"));
+            this.url = properties.getProperty("url");
+            this.user = properties.getProperty("user");
+            this.password = properties.getProperty("password");
+            this.connection = DriverManager.getConnection(this.url, this.user, this.password);
+        } catch(SQLException e){
+            e.getMessage();
+        } catch(IOException e){
+            e.getMessage();
+        }
+
+    }
+
+    public void close(){
+        try{
+            this.connection.close();
+        }catch(SQLException e){
+
+        }
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
