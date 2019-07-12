8c4c8544b678dd417bbcd9bff0f1f6778d974085
Sean McClanahan <s.mcclanahan96@gmail.com>
2019-07-12 10:16:15 -0500

    Major updates to Basic User class

---
+++ src/main/java/com/revature/BasicUser.java
@@ -0,0 +1,96 @@
+package com.revature;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class BasicUser implements Commands {
+    private int UserID;
+    Connection connection;
+
+    public BasicUser() {
+    }
+
+    public BasicUser(String Username, Connection connection) { //constructor to set up connection information
+        this.connection = connection;
+        
+        try (PreparedStatement pstatement = connection.prepareStatement(" select id from userlogins where username = ? ")) {
+           pstatement.setString(1, Username);
+           ResultSet resultSet = pstatement.executeQuery();
+           this.UserID = resultSet.getInt("id");
+           System.out.println(this.UserID);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public double Balance(String user, String acctType) { //attempts to retrieve balance from sql database
+        try { //begin try block
+            PreparedStatement pstatement = this.connection.prepareStatement( "select balance from accounts where userfk = ?" );
+            pstatement.setInt(1, this.UserID);
+            ResultSet resultSet =  pstatement.executeQuery();
+            return resultSet.getDouble("balance"); //returns current balance on acct from sql database
+
+        } catch (Exception e) { //begin catch block
+            e.printStackTrace();
+        } //end try, catch block
+        System.err.println("Attempt to retrieve amount failed"); //error message
+        return -1; //error return key
+    } //end Balance
+
+    @Override
+    public double Deposit(String user, String acctType, float amount) { //attempts to update sql database with new deposit information
+        PreparedStatement pstatement;
+        try { //begin try block
+            pstatement = this.connection.prepareStatement("select balance from accounts where userfk = ? and accttype = ?");
+            pstatement.setInt(1, this.UserID);
+            pstatement.setString(2, acctType);
+            ResultSet resultSet = pstatement.executeQuery();
+            double currentBalance = resultSet.getDouble("balance"); //gets current balance from sql database
+            pstatement = this.connection.prepareStatement("update accounts set balance = ? where userfk = ? ");
+            pstatement.setDouble(1, currentBalance + amount);
+            pstatement.setInt(2, this.UserID);
+            pstatement.execute(); //execute update
+            return currentBalance + amount; //returns new balance
+
+        } catch (SQLException e) { //begin catch block
+            e.printStackTrace();
+        } //end try, catch block
+        System.err.println("Deposit Failed"); //error message
+        return -1; //error return key
+    } //end Deposit
+
+    @Override
+    public double Withdrawl(String user, String acctType, float amount) {
+        PreparedStatement pstatement;
+        try { //begin try block
+            pstatement = this.connection.prepareStatement("select balance from accounts where userfk = ? and accttype = ?");
+            pstatement.setInt(1, this.UserID);
+            pstatement.setString(2, acctType);
+            ResultSet resultSet = pstatement.executeQuery();
+            double currentBalance = resultSet.getDouble("balance"); //gets current balance from sql database
+            pstatement = this.connection.prepareStatement("update accounts set balance = ? where userfk = ? ");
+            pstatement.setDouble(1, currentBalance + amount);
+            pstatement.setInt(2, this.UserID);
+            pstatement.execute(); //execute update
+            return currentBalance - amount; //returns new balance
+
+        } catch (SQLException e) { //begin catch block
+            e.printStackTrace();
+        } //end try, catch block
+        System.err.println("Withdrawl Failed"); //error message
+        return -1; //error return kek
+    } //end Withdrawl
+
+    @Override
+    public void Close(String user, String acctType) { //function for close of connection
+        try { //begin try block
+            this.connection.close(); //closes connection to database
+        } catch (SQLException e) { //begin catch block
+            e.printStackTrace();
+            System.err.println("Connection refused to close"); //error message
+        } //end try, catch block
+    } //end Close
+} //end class BasicUser
--- src/main/java/com/revature/Commands.java
+++ src/main/java/com/revature/Commands.java
@@ -1,9 +1,9 @@
 package com.revature;
 
 interface Commands {
-    int Balance(String user, String acctType);
-    void Deposit(String user, String acctType, float amount);
-    void Withdrawl(String user, String acctType, float amount);
+    double Balance(String user, String acctType);
+    double Deposit(String user, String acctType, float amount);
+    double Withdrawl(String user, String acctType, float amount);
     void Close(String user, String acctType);
 
 }
